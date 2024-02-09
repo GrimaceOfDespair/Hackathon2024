d677cf48f541c1797ec04477869f4c41c6d6348d
LesterParedisPXL <Lester.Paredis@student.pxl.be>
2024-02-09 16:08:58 +0100

    testen

--- Hackathon2024/TemplateRenderer.cs
+++ Hackathon2024/TemplateRenderer.cs
@@ -4,47 +4,33 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 public static class ExpressionTransformer
 {
+    private static readonly Regex ExpressionPattern = new Regex(@"\[%(.+?)%\]", RegexOptions.Compiled);
+
     public static string RenderExpressions(string content, string baseUrl, Dictionary<string, object> data = null)
     {
         if (string.IsNullOrEmpty(content))
             return content;
 
-        StringBuilder result = new StringBuilder(content.Length);
-        int currentIndex = 0;
+        var matches = ExpressionPattern.Matches(content);
+        var result = new StringBuilder(content.Length);
+        int lastIndex = 0;
 
-        while (currentIndex < content.Length)
+        foreach (Match match in matches)
         {
-            int expressionStartIndex = content.IndexOf("[%", currentIndex, StringComparison.Ordinal);
-            if (expressionStartIndex == -1)
-            {
-                result.Append(content, currentIndex, content.Length - currentIndex);
-                break;
-            }
-
-            int expressionEndIndex = content.IndexOf("%]", expressionStartIndex + 2, StringComparison.Ordinal);
-            if (expressionEndIndex == -1)
-            {
-                result.Append(content, expressionStartIndex, content.Length - expressionStartIndex);
-                break;
-            }
-
-            int expressionLength = expressionEndIndex - expressionStartIndex - 2;
-
-            result.Append(content, currentIndex, expressionStartIndex - currentIndex);
-
-            string expression = content.Substring(expressionStartIndex + 2, expressionLength);
+            result.Append(content, lastIndex, match.Index - lastIndex);
+            string expression = match.Groups[1].Value;
             string processedExpression = ProcessExpression(expression, baseUrl, data);
             result.Append(processedExpression);
-
-            currentIndex = expressionEndIndex + 2;
+            lastIndex = match.Index + match.Length;
         }
 
+        result.Append(content, lastIndex, content.Length - lastIndex);
         return result.ToString();
     }
-
 
     private static string ProcessExpression(string expression, string baseUrl, Dictionary<string, object> data)
     {
@@ -64,7 +50,6 @@ public static class ExpressionTransformer
 
         return "";
     }
-
 }
 
 public class TemplateRenderer
@@ -84,27 +69,27 @@ public class TemplateRenderer
 
     private void ProcessRepeaterNodes(HtmlDocument document, string baseUrl, Dictionary<string, Dictionary<string, object>[]> allData)
     {
-        var repeaterItemNodes = document.DocumentNode.DescendantsAndSelf("sg:repeateritem").ToList();
-        foreach (var repeaterItemNode in repeaterItemNodes)
+        var repeaterNodes = document.DocumentNode.Descendants("sg:repeater").ToList();
+        foreach (var repeaterNode in repeaterNodes)
         {
-            // Retrieve the data selection once for each repeater item
-            var dataSelection = repeaterItemNode.GetAttributeValue("dataselection", "");
-            var repeaterItemContent = repeaterItemNode.InnerHtml;
-
-            // Check if the required data exists
-            if (!allData.TryGetValue(dataSelection, out var data))
+            var repeaterItemNodes = repeaterNode.Descendants("sg:repeateritem").ToList();
+            foreach (var repeaterItemNode in repeaterItemNodes)
             {
-                continue; // Skip this iteration if data is not found
-            }
+                var dataSelection = repeaterNode.GetAttributeValue("dataselection", "");
+                var repeaterItemContent = repeaterItemNode.InnerHtml;
 
-            var repeatedContent = new StringBuilder();
-            foreach (var dataItem in data)
-            {
-                var result = ExpressionTransformer.RenderExpressions(repeaterItemContent, baseUrl, dataItem);
-                repeatedContent.AppendLine(result); // Append with a newline
-            }
+                var repeatedContent = new StringBuilder();
+                if (allData.TryGetValue(dataSelection, out var data))
+                {
+                    foreach (var dataItem in data)
+                    {
+                        var result = ExpressionTransformer.RenderExpressions(repeaterItemContent, baseUrl, dataItem);
+                        repeatedContent.Append(result);
+                    }
+                }
 
-            ReplaceHtml(repeaterItemNode, repeatedContent);
+                ReplaceHtml(repeaterNode, repeatedContent);
+            }
         }
     }
 
